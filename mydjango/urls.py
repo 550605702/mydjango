04de46293fb1c9d6326deb385c69204735354941
550605702 <550605702@qq.com>
2020-11-12 10:22:09 +0800

    项目框架搭建+注释

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from project import views
 
+# 地址配置文件
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    #两种写法都行，配置url访问地址
+    # url(r'^$', views.index),
 ]
---
+++ project/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    # -----中间放调用接口获得参数-------
+    # -----中间放调用接口获得参数-------
+
+    #直接返回一段文字
+    # return HttpResponse('hello word')
+    #返回html地址链接，html存在project包的templates文件夹，系统会自动识别
+    return render(request,'test.html')
